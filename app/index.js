2026f2687b82eb7e9badd576c878a19da3cf60c4
Elie Rotenberg <elie@rotenberg.io>
2015-12-19 13:19:43 +0100

    Export ReactML components
    ReactML base components are now exported so that they can be used eg. by
    ReactMLEditor.

--- app/index.js
+++ app/index.js
@@ -2,7 +2,30 @@ import 'babel-polyfill';
 import compile from './compile';
 import presets from './presets';
 
+import ReactMLBold from './components/ReactMLBold';
+import ReactMLCode from './components/ReactMLCode';
+import ReactMLFragment from './components/ReactMLFragment';
+import ReactMLImage from './components/ReactMLImage';
+import ReactMLItem from './components/ReactMLItem';
+import ReactMLLink from './components/ReactMLLink';
+import ReactMLList from './components/ReactMLList';
+import ReactMLParagraph from './components/ReactMLParagraph';
+import ReactMLQuote from './components/ReactMLQuote';
+import ReactMLStrikethrough from './components/ReactMLStrikethrough';
+import ReactMLUnderline from './components/ReactMLUnderline';
+
 export default {
   compile,
   presets,
+  ReactMLBold,
+  ReactMLCode,
+  ReactMLFragment,
+  ReactMLImage,
+  ReactMLItem,
+  ReactMLLink,
+  ReactMLList,
+  ReactMLParagraph,
+  ReactMLQuote,
+  ReactMLStrikethrough,
+  ReactMLUnderline,
 };
